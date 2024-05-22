0e67dc3de6a2a6e71a020cdc14ce8c6b769622f4
MohammadAliJamei <mohammadalijamei995@gmail.com>
2024-05-22 20:29:02 +0330

    categories list added.

---
+++ src/features/categories/components/category-list.jsx
@@ -0,0 +1,36 @@
+const CategoryList = ({categories: {data, totalRecords}}) => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>نام</th>
+                  <th>عملیات</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data.map((category) => {
+                    return (
+                      <tr key={category.id}>
+                        <td>{category.name}</td>
+                        <td className="table-action">
+                          <a href="" className="ms-3 d-inline-block">حذف</a>  
+                          <a href="" className="ms-3 d-inline-block">ویرایش</a>  
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default CategoryList;
--- src/pages/course-categories.jsx
+++ src/pages/course-categories.jsx
@@ -1,6 +1,40 @@
+import { Await, defer, useLoaderData } from "react-router-dom";
+import { httpTnterceptedService } from "@core/http-service";
+import CategoryList from "../features/categories/components/category-list";
+import { Suspense } from "react";
+
 const CourseCategories = () => {
+  const data = useLoaderData()
   return (
-    <h2>course categories</h2>
+    <div className="reow">
+    <div className="col-12">
+      <div className="d-flex aling-items-center justify-contents-between mb-5">
+        <a className="btn btn-primary fw-bolder mt-n1">
+          افزودن دسته جدید
+        </a>
+      </div>
+      <Suspense fallback={<p className="test-info">درحال لود اطلاعات ...</p>}>
+        <Await resolve={data.categories}>
+          {
+            (loadedCategories) => <CategoryList categories={loadedCategories}/>
+          }
+        </Await>
+      </Suspense>
+      
+    </div>
+  </div>
   )
 }
+
+export async function categoriesLoader() {
+  return defer ({
+    categories: loadCategories()
+  })
+}
+
+const loadCategories = async () => {
+  const response = await httpTnterceptedService.get('/CourseCategory/sieve');
+  return response.data;
+}
+
 export default CourseCategories;
