0f62d1e5ff6fe220ff2b8649cdb227820773169a
MohammadAliJamei <mohammadalijamei995@gmail.com>
2024-05-22 19:42:14 +0330

    course details added.

--- src/features/courses/components/course.jsx
+++ src/features/courses/components/course.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+
 const Course = ({
+  id,
   title,
   coverImageUrl,
   courseLevel,
@@ -8,13 +11,15 @@ const Course = ({
 }) => {
   return (
     <div className="card">
-      <img className="card-img-top" src={coverImageUrl}/>
+      <img className="card-img-top" src={coverImageUrl} />
       <div className="card-header px-4 pt-4 pb-0">
         <div className="badge bg-primary my-2 fw-bolder">
           {courseLevel}
         </div>
         <h4 className="mb-0">
-          {title}
+          <Link to={`/courses/${id}`}>
+            {title}
+          </Link>
         </h4>
       </div>
       <div className="card-body px-4 pt-2">
@@ -23,7 +28,7 @@ const Course = ({
         </p>
       </div>
       <div className="card-footer fs-sm d-flex align-items-center fw-bolder text-secondary justify-content-between">
-      <div className="d-flex algn-items-center gap-1">
+        <div className="d-flex algn-items-center gap-1">
           {`${duration} ساعت`}
         </div>
         <div className="d-flex algn-items-center gap-1">
