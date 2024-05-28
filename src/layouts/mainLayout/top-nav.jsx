df546d9b2061f09b4a929d9150b83f885407da6d
Mohammad Ali <ma.jamei@raymon.info>
2024-05-28 14:55:34 +0330

    logout added.

--- src/layouts/mainLayout/top-nav.jsx
+++ src/layouts/mainLayout/top-nav.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import ChangeLanguage from "../../components/change-language";
 import ChangeTheme from "../../components/change-theme";
 import { useAppContext } from "../../contexts/app/app-context";
@@ -5,14 +6,30 @@ import avatar from '@assets/images/avatar.jpg';
 
 const TopNav = () => {
   const {toggleSidebar, language} = useAppContext()
+
+  const navigate = useNavigate()
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  }
+
   return (
     <nav className="navbar navbar-expand navbar-light navbar-bg">
     <a className="sidebar-toggle" onClick={toggleSidebar}>
       <i className="hamburger align-self-center"></i>
     </a>
-    <div className="d-flex align-items-center gap-3">
+    <div className="d-flex align-items-center gap-3 me-3">
       <ChangeLanguage />
       <ChangeTheme />
+    </div>
+    <div className={`${language === 'fa' ? 'me-auto' : 'ms-auto' }`}>
+      <button
+        className="btn ms-2 btn-outline-danger fw-bolder"
+        onClick={logout}
+      >
+        خارج شوید
+      </button>
     </div>
     <img src={avatar} className={`avatar img-fluid rounded-circle ${language === 'fa' ? 'me-auto' : 'ms-auto'}`} />
   </nav>
